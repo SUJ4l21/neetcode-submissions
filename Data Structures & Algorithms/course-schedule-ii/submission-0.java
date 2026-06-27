20c2cc0b0db7c392d3f50c37ed14f82389a6009d
SUJ4l21 <168823412+SUJ4l21@users.noreply.github.com>
2026-06-27 18:34:06 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int m =  prerequisites.length;
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int []indegree =new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        } 
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)q.add(i);
+        }
+        List<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+            topo.add(node);
+
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0)q.add(it);
+            }
+        }
+
+        if(topo.size()==numCourses)return topo.stream()
+              .mapToInt(Integer::intValue)
+              .toArray();
+        return new int[]{};
+    }
+}
