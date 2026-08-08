67ca71213c15781b27a1a6905f32bd2f1ae3bc9d
sujal <168823412+SUJ4l21@users.noreply.github.com>
2026-08-08 11:13:01 +0530

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int [] arr = new int[n+2];
+        int [][] dp = new int[n+3][n+3];
+        for(int []a : dp){
+            Arrays.fill(a,-1);
+        }
+        arr[0]=1;
+        arr[arr.length-1]=1;
+        for(int i = 0;i<n;i++){
+            arr[i+1] = nums[i];
+        }
+        return helper (1,n,arr,dp);
+    }
+    private int helper(int i,int j ,int [] arr,int [][]dp){
+        if(i>j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int max = Integer.MIN_VALUE;
+        for(int k=i;k<=j;k++){
+            int temp = arr[i-1]*arr[k]*arr[j+1] + helper(i,k-1,arr,dp)+helper(k+1,j,arr,dp);
+            max = Math.max(max,temp);
+        }
+        return dp[i][j] = max;
+    }
+}
