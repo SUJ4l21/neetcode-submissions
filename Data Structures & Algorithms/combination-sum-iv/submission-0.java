dfbb5b21c71ddbf487bb43ade16f7abdeea8b858
sujal <168823412+SUJ4l21@users.noreply.github.com>
2026-08-10 10:14:29 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+        int [] dp = new int[target+1];
+        Arrays.fill(dp,-1);
+        return helper(nums,target,dp);
+    }
+    private int helper(int[]nums, int target,int []dp){
+        if(target==0)return 1;
+        if(target<0)return 0;
+        if(dp[target]!=-1)return dp[target];
+        int ans =0;
+        for(int k = 0;k<nums.length;k++){
+            if(nums[k]<=target){
+                ans+= helper(nums,target-nums[k],dp);
+            }
+        }
+        return dp[target] = ans;
+            
+    }
+}
